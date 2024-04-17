549953faf8e8e0a3f8b45fc86cec70f1fc8e5699
annahylbert05 <anna05@stanford.edu>
2024-04-17 15:07:23 -0700

    Added Orbit Propagators
    Added 2 files:
    * numerical_orbit_propagation uses a numerical solver that we could later modify to account for perturbations
    * orbit_propagator_simulink uses simulink to vary the mean anomaly across a given timestep to propagate the orbit

---
+++ numerical_orbit_propagation.m
@@ -0,0 +1,88 @@
+
+% Constants
+rE = 6378.1; % km
+AU = 1.496e8; % 1 AU in km
+mu = 3.986004418e5; % Gravitational parameter of the Earth in km^3/s^2
+
+% Define initial conditions (orbital elements)
+a = 7080.6; % Semi-major axis (in km)
+e = 0.0000979;   % Eccentricity
+i = deg2rad(98.2); % Inclination (in radians)
+omega = deg2rad(120.4799); % Arguement of perigee (in radians)
+Omega = deg2rad(95.2063); % Ascending node (in radians)
+nu = 0; % True Anomaly (in radians)
+
+% Calculate orbital period
+T = 2*pi*sqrt((a)^3 / mu); % Orbital period in seconds
+
+% Define time span for propagation
+t_span = [0 T]; % Propagate for 10000 seconds
+
+% Define initial conditions (position and velocity vectors)
+[r0, v0] = keplerian2ECI(a, e, i, Omega, omega, nu, mu);
+
+% Define function for orbital equations of motion
+orbital_eqns = @(t, y) orbital_derivatives(t, y, mu);
+
+% Use ode45 solver to propagate orbit
+[t, y] = ode45(orbital_eqns, t_span, [r0; v0]);
+
+% Extract position and velocity vectors from solution
+r = y(:, 1:3);
+v = y(:, 4:6);
+
+% Plot orbit in 3D
+plot_orbit(r, rE);
+
+function [rECI, vECI] = keplerian2ECI(a, e, i, Omega, omega, nu, mu)
+
+    % Calculate position and velocity in the perifocal coordinate system
+    E = acos((e + cos(nu))/(1+(e*cos(nu))));
+    n = sqrt(mu/a^3);
+    r_perifocal = [a*(cos(E) - e); a*sin(E)*sqrt(1-e^2); 0];
+    v_perifocal = a*n/(1-e*cos(E)) * [-sin(E); cos(E)*sqrt(1-e^2); 0];
+
+    % Rotation matrices
+    R3_Omega = [cos(-Omega) sin(-Omega) 0;
+                -sin(-Omega) cos(-Omega) 0;
+                0 0 1];
+    R1_i = [1 0 0;
+            0 cos(-i) sin(-i);
+            0 -sin(-i) cos(-i)];
+    R3_omega = [cos(-omega) sin(-omega) 0;
+                -sin(-omega) cos(-omega) 0;
+                0 0 1];
+
+    % Transformation to ECI
+    Q_perifocal2ECI = R3_Omega * R1_i * R3_omega;
+    rECI = Q_perifocal2ECI * r_perifocal;
+    vECI = Q_perifocal2ECI * v_perifocal;
+end
+
+function dydt = orbital_derivatives(~, y, mu)
+    % Define the equations of motion for orbital derivatives
+    r = y(1:3);
+    v = y(4:6);
+    r_mag = norm(r);
+    dvdt = -mu * r / r_mag^3;
+    drdt = v;
+    dydt = [drdt; dvdt];
+end
+
+function plot_orbit(r, rE)
+    % Plot orbit in 3D
+    figure;
+    plot3(r(:,1), r(:,2), r(:,3), 'r', 'LineWidth', 2.5);
+    hold on;
+    xlabel('X (km)');
+    ylabel('Y (km)');
+    zlabel('Z (km)');
+    title('Aqua Orbit');
+    grid on;
+
+    [xE, yE, zE] = ellipsoid(0,0,0,rE, rE, rE, 20);
+    surface(xE, yE, zE, 'FaceColor', 'blue', 'EdgeColor', 'black')
+    
+    axis equal;
+    view(3);
+end
---
+++ orbit_propogator_simulink.m
@@ -0,0 +1,98 @@
+% Constants
+rE = 6378.1; % km
+AU = 1.496e8; % 1 AU in km
+mu_earth = 3.986004418e5; % Gravitational parameter of the Sun in km^3/s^2
+
+% Initial Conditions
+e_float = 0.0000979; % eccentricity 
+a_float = 7080.6; % km
+i_float = 98.2; %degrees
+omega_float = 120.4799; % arguement of perigee // degrees
+Omega_float = 95.2063; % ascending node // degrees
+
+a = [[0, a_float],
+    [10000000, a_float]];
+e = [[0, e_float],
+    [10000000, e_float]];
+i = [[0, i_float],
+    [10000000, i_float]];
+omega = [[0, omega_float],
+    [10000000, omega_float]];
+Omega = [[0, Omega_float],
+    [10000000, Omega_float]];
+
+% Calculate orbital period
+T = 2*pi*sqrt((a_float)^3 / mu_earth); % Orbital period in seconds
+T_minutes = T / (60); % Convert to Earth years
+
+% Calculate mean motion
+n_float = 2*pi / T; % Mean motion in rad/s
+n = [[0, n_float],
+    [10000000, n_float]];
+
+
+fprintf('Orbital Period (T): %.2f seconds\n', T);
+fprintf('Orbital Period (T): %.2f minutes\n', T_minutes);
+fprintf('Mean Motion (n): %f rad/s\n', n_float);
+disp(n)
+
+% Plot orbit
+figure;
+plot3(out.x.Data, out.y.Data, out.z.Data, 'r', 'LineWidth', 1.5);
+hold on;
+
+[xE, yE, zE] = ellipsoid(0,0,0,rE, rE, rE, 20);
+surface(xE, yE, zE, 'FaceColor', 'blue', 'EdgeColor', 'black')
+
+% Label axes and include units
+xlabel('X (km)');
+ylabel('Y (km)');
+zlabel('Z (km)');
+title('Aqua Orbit');
+grid on;
+axis equal;
+view(4);
+
+function E = solveKeplersEquation(M, e, epsilon)
+    E = M; % Initial Guess
+    while true
+        E_new = E - (E - e*sin(E) - M) / (1 - e*cos(E)); % Newton-Raphson iteration
+        if abs(E_new - E) < epsilon
+            break; % Convergence reached
+        end
+        E = E_new;
+    end
+end
+
+function [x,y,z]  = keplerian2ECI(a, e, i, Omega, omega, E)
+    % Constants
+    mu = 3.986e5; % Earth's gravitational parameter (km^3/s^2)
+
+    % Convert orbital elements from degrees to radians
+    i = deg2rad(i);
+    Omega = deg2rad(Omega);
+    omega = deg2rad(omega);
+
+    % Calculate position and velocity in the perifocal coordinate system
+    n = sqrt(mu/a^3);
+    r_perifocal = [a*(cos(E) - e); a*sin(E)*sqrt(1-e^2); 0];
+    v_perifocal = a*n/(1-e*cos(E)) * [-sin(E); cos(E)*sqrt(1-e^2); 0];
+
+    % Rotation matrices
+    R3_Omega = [cos(-Omega) sin(-Omega) 0;
+                -sin(-Omega) cos(-Omega) 0;
+                0 0 1];
+    R1_i = [1 0 0;
+            0 cos(-i) sin(-i);
+            0 -sin(-i) cos(-i)];
+    R3_omega = [cos(-omega) sin(-omega) 0;
+                -sin(-omega) cos(-omega) 0;
+                0 0 1];
+
+    % Transformation to ECI
+    Q_perifocal2ECI = R3_Omega * R1_i * R3_omega;
+    rECI = Q_perifocal2ECI * r_perifocal;
+    x = rECI(1);
+    y = rECI(2);
+    z = rECI(3);
+end
