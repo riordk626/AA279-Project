cfbdcc59dc98a24f4c5373611d1010d93ed0fb37
riordk626 <riordk@stanford.edu>
2024-05-25 15:23:08 -0700

    finished testing new initialization and plotting

--- Code/src/starTrackerDataInit.m
+++ Code/src/starTrackerDataInit.m
@@ -1,6 +1,7 @@
 clc, clear, close all
 
 rng(10)
+wmag = 0.3;
 
 Vstar = rand([3 2]);
 for i=1:size(Vstar,2)
@@ -9,9 +10,9 @@ end
 
 wstar = rand([2 1]);
 
-save('starTrackerSimpleUndersampled.mat', "Vstar", "wstar")
+save('attitudeEdgeMeasData.mat', "Vstar", "wstar", "wmag")
 
-clear Vstar
+clear Vstar wstar
 
 Vstar = rand([3 10]);
 for i=1:size(Vstar,2)
@@ -19,7 +20,5 @@ for i=1:size(Vstar,2)
 end
 
 wstar = (1e-2).*rand([10 1]) + 0.9;
-
-wmag = 0.3;
 
 save('attitudeMeasData.mat', "Vstar", "wstar", "wmag")
