3fcc66d9e5e5bccbe21b2170227fdb553b00f106
riordk626 <riordk@stanford.edu>
2024-06-03 17:00:36 -0700

    began modeling reaction wheel actuators

---
+++ Code/src/initActuatorModel.m
@@ -0,0 +1,12 @@
+function initActuatorModel(controlMoment, actuatorParams)
+model = 'actuatorModel';
+
+load_system(model)
+mws = get_param(model, 'modelworkspace');
+mws.assignin(controlMoment)
+
+switch controlMoment
+    case "reactionWheel"
+        mws.assignin('Lw0', actuatorParams.Lw0)
+        mws.assignin('A', actuatorParams.A)
+end
