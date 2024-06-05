8466e98793a4ac21d0ac873442f3472ba1a34ded
riordk626 <riordk@stanford.edu>
2024-06-05 00:08:14 -0700

    potentially finished both actuator models; formatting/outlier issue needs to be addressed for magnetorquer + wheel combo

--- Code/src/HW9main.m
+++ Code/src/HW9main.m
@@ -3,7 +3,7 @@ clc, clear
 close all
 
 projectStartup;
-exportflag = true;
+exportflag = false;
 figurePath = '../../Images/PS9/';
 
 [rcm, Itotal_b, Itotal_p, A_ptob] = aquaMassProps();
@@ -50,14 +50,6 @@ controlLawStruct.errorType = "small";
 controlLawStruct.controllerParams = struct([]);
 controlLawStruct.dt_cont = 1e-1;
 
-Lw0 = 10;
-A = (1/sqrt(3)).*[-1, 1, 1, -1;
-                  -1, -1, 1, 1;
-                  1, 1, 1, 1];
-actuatorModelStruct.controlMoment = "reactionWheel";
-actuatorModelStruct.actuatorParams.Lw0 = Lw0.*ones([4 1]);
-actuatorModelStruct.actuatorParams.A = A;
-
 ICstruct.r0 = r0; ICstruct.v0 = v0;
 
 Tfinal = 3*T;
@@ -83,9 +75,57 @@ R0 = R_RTNtoPdes * R_ECItoRTN;
 % 
 ICstruct.om0 = om0; ICstruct.R0 = R0;
 
+% initialize for reaction wheel test
+Lw0 = 10;
+A = (1/sqrt(3)).*[-1, 1, 1, -1;
+                  -1, -1, 1, 1;
+                  1, 1, 1, 1];
+actuatorModelStruct.controlMoment = "reactionWheel";
+actuatorModelStruct.actuatorParams.Lw0 = Lw0.*ones([4 1]);
+actuatorModelStruct.actuatorParams.A = A;
 
 simIn = initAqua(Tfinal, R_RTNtoPdes, ICstruct, orbitStruct, plantStruct,...
     distStruct,sensorStruct,kalmanFilterStruct,controlLawStruct, actuatorModelStruct);
 
 simOut = sim(simIn);
 
+t = simOut.t;
+R_ItoP = simOut.yout{1}.Values.Data;
+Mc = squeeze(simOut.Mc.Data);
+Mout = squeeze(simOut.Mout.Data);
+Lwdot = squeeze(simOut.Lwdot.Data);
+values = {Mc;Mout;Lwdot};
+valueNames = {'$M_c$ [Nm]';'$M_{out}$ [Nm]';'$\dot{L}_w$ [Nm]'};
+valueLabels = {{'$M_x$';'$M_y$';'$M_z$'}, {'$M_x$';'$M_y$';'$M_z$'}, {'$\dot{L}_1$'...
+    ;'$\dot{L}_2$';'$\dot{L}_3$';'$\dot{L}_4$'}};
+figureName = [figurePath, 'reaction_wheel_model_output.png'];
+
+fig = figure();
+timeHistoryPlot(fig, t, values, valueNames, valueLabels, figureName, true, exportflag)
+
+% initialize for magnetorquer test
+actuatorModelStruct.controlMoment = "magnetorquer";
+
+simIn = initAqua(Tfinal, R_RTNtoPdes, ICstruct, orbitStruct, plantStruct,...
+    distStruct,sensorStruct,kalmanFilterStruct,controlLawStruct, actuatorModelStruct);
+
+simOut = sim(simIn);
+
+t = simOut.t;
+R_ItoP = simOut.yout{1}.Values.Data;
+Mc = squeeze(simOut.Mc.Data);
+Mout = squeeze(simOut.Mout.Data);
+m = squeeze(simOut.m.Data);
+Ldot = simOut.Ldot.Data;
+values = {Mc;Mout;m(1:2,:)};
+valueNames = {'$M_c$ [Nm]';'$M_{out}$ [Nm]';'$m$'};
+valueLabels = {{'$M_x$';'$M_y$';'$M_z$'}, {'$M_x$';'$M_y$';'$M_z$'}, {'$m_x$'...
+    ;'$m_y$'}};
+figureName = [figurePath, 'simple_magnetorquer_model_output.png'];
+
+fig = figure();
+timeHistoryPlot(fig, t, values, valueNames, valueLabels, figureName, true, exportflag)
+
+fig;
+subplot(3,1,3)
+ylim([-0.5 0.5])
--- Code/src/initActuatorModel.m
+++ Code/src/initActuatorModel.m
@@ -9,4 +9,9 @@ switch controlMoment
     case "reactionWheel"
         mws.assignin('Lw0', actuatorParams.Lw0)
         mws.assignin('A', actuatorParams.A)
+    case "magnetorquer"
+        load("magConstants.mat", "mhat_ecef", "B0", "Re")
+        mws.assignin('mhat_ecef', mhat_ecef)
+        mws.assignin('B0', B0)
+        mws.assignin('Re', Re)
 end
